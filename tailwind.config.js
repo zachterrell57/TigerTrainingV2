79d5a9f0c57f141b262eebc5c6e28da9f5ba99cb
Zachary Terrell <zachterrell@gmail.com>
2022-04-15 16:23:26 -0500

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'main-gold': 'rgb(169, 143, 66)',
+      },
+      backgroundImage: {
+        'field-bg': 'url("/img/field.jpeg")',
+        'track-bg': 'url("/img/track.jpeg")',
+        'sprinter-bg': 'url("/img/sprinter.jpeg")',
+      },
+    }
+  },
+  plugins: [],
+}
